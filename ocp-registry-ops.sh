c5f0e113ac6a31dd9d037bcc9527cf6c659de3ca
cheevatee <38664398+cheevatee@users.noreply.github.com>
2020-02-18 22:22:53 +0700

    Create ocp-registry-ops.sh

---
+++ ocp-registry-ops.sh
@@ -0,0 +1,15 @@
+rm -rf dockerImageReference.txt
+rm -rf dockerImageStreamTag.txt
+
+oc login https://api.ocp.example.com:6443 -u kubeadmin -p XXXXX-XXXXX-XXXXX-XXXXX
+for i in $(oc get images -o jsonpath='{range .items[*]}{.dockerImageReference} {.dockerImageMetadata.Created} {.dockerImageMetadata.Size}{"\n"}{end}'|sed 's/@/ /'|sort -k1,1 -k3,3|egrep -v "registry.redhat.io|quay.io"|awk '{print $1}'|uniq)
+do 
+    oc get images -o jsonpath='{range .items[*]}{.dockerImageReference} {.dockerImageMetadata.Created} {.dockerImageMetadata.Size}{"\n"}{end}'|sed 's/@/ /'|sort -r -k1,1 -k3,3|grep -w ^$i|tail -n +4|awk '{print $2}' >> dockerImageReference.txt
+    oc get images -o jsonpath='{range .items[*]}{.dockerImageReference} {.dockerImageMetadata.Created} {.dockerImageMetadata.Size}{"\n"}{end}'|sed 's/@/ /'|sort -r -k1,1 -k3,3|grep -w ^$i|tail -n +4|awk '{print $2}'|xargs -i oc delete image {}
+done
+
+for i in $(cat dockerImageReference.txt)
+do 
+    oc get imagestreamtag --all-namespaces|grep -i $i >> dockerImageStreamTag.txt
+    oc get imagestreamtag --all-namespaces|grep -i $i|awk '{ system("oc delete istag/"$2" -n "$1) }'
+done
