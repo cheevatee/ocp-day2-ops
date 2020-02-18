5202d2680075203dad4b3f4b46aecd40ca3859d8
cheevatee <38664398+cheevatee@users.noreply.github.com>
2020-02-18 22:20:49 +0700

    Update ocp-registry-ops.sh

--- ocp-registry-ops.sh
+++ ocp-registry-ops.sh
@@ -1,7 +1,7 @@
 rm -rf dockerImageReference.txt
 rm -rf dockerImageStreamTag.txt
 
-oc login https://api.ocp.nonpro.dsl:6443 -u kubeadmin -p DaSmC-icqXi-PXYwm-ui9E5
+oc login https://api.ocp.example.com:6443 -u kubeadmin -p XXXXX-XXXXX-XXXXX-XXXXX
 for i in $(oc get images -o jsonpath='{range .items[*]}{.dockerImageReference} {.dockerImageMetadata.Created} {.dockerImageMetadata.Size}{"\n"}{end}'|sed 's/@/ /'|sort -k1,1 -k3,3|egrep -v "registry.redhat.io|quay.io"|awk '{print $1}'|uniq)
 do 
     oc get images -o jsonpath='{range .items[*]}{.dockerImageReference} {.dockerImageMetadata.Created} {.dockerImageMetadata.Size}{"\n"}{end}'|sed 's/@/ /'|sort -r -k1,1 -k3,3|grep -w ^$i|tail -n +4|awk '{print $2}' >> dockerImageReference.txt
